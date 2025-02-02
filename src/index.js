3e14811ab527b8c34768e6b7eba76c4970482e61
AlonJoshua <alonjoshua@gmail.com>
2025-02-02 23:56:02 +0200

    add cart context

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { UserProvider } from "./context/user.context";
 import { ProductsProvider } from "./context/products.context";
+import { CartProvider } from "./context/cart.context";
 
 import "./index.css";
 
@@ -13,13 +14,15 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-      <ProductsProvider>
-          <App />
-      </ProductsProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
